d3b42dfede68c3d196b19d71eccc0f1418d8686b
Pratyush Mittal <pratyushmittal@gmail.com>
2019-12-15 15:45:33 +0530

    Upgrading Pelican to 4
    Upgrading Pelican is actually not tough.
    http://docs.getpelican.com/en/stable/install.html#upgrading
    
    Basic steps:
    - Use pip to update pelican
    - Create new git branch
    - Remove everything (using finder)
    - Use `pelican-quickstart`
    - Diff the configs
    - Restore content and output folder
    - Generate content
    - Diff the output
    - Merge
    - Push
    
    Usually there are very limited breaking changes.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,33 +1,43 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Pratyush Mittal'
-SITENAME = u'A Fully-Faltoo Blog'
-SITEURL = 'http://fully-faltoo.com'
+AUTHOR = 'Pratyush Mittal'
+SITENAME = 'Blog of Faltoo Things'
+SITEURL = ''
+
+PATH = 'content'
 
 TIMEZONE = 'Asia/Kolkata'
 
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ('Hacker News',
-        "To keep updated with the latest tech developments",
-        "http://news.ycombinator.com/"),
-    ('LifeHacker',
-        "Collection of the best life hacks",
-        "http://lifehacker.com"),
-    ('Screener.in',
-        'A Stock Screener and stock analysis tool I am working on',
-        'http://www.screener.in'),
-    ('WebTrickz',
-        "My cousin Mayur's Blog for the best 'web-tricks'",
-        'http://www.webtrickz.com'),
+    (
+        'Screener.in',
+        'This is where I spend most of my time',
+        'https://www.screener.in'
+    ), (
+        'Pinboard',
+        'What I am reading?',
+        'https://pinboard.in/u:pratyush',
+    ), (
+        'StackOverflow',
+        'Questions and answers',
+        'https://stackoverflow.com/users/232649/pratyush',
+    ), (
+        'Twitter',
+        'This is where I follow my heroes',
+        'https://twitter.com/faltoo',
+    ),
 )
 
 # Social widget
@@ -37,26 +47,27 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 7
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = False
+#RELATIVE_URLS = True
 
+# OWN SETTINGS FROM HERE
+# Options are given here: http://docs.getpelican.com/en/stable/settings.html
 
-# Blog Specific
-TAGLINE = "Pratyush's Diary"
-# :Menus
-DISPLAY_PAGES_ON_MENU = True
-# :Navigation Menu
-MENUITEMS = (('@faltoo', 'http://twitter.com/faltoo'),)
-
-
-# Author
-AUTHOR_SAVE_AS = False
-ABOUT_AUTHOR = ("My name is Pratyush Mittal. "
-                "I am a Chartered Accountant living in Lucknow, India. "
-                "I love to create financial web-applications and "
-                "write Python programs. "
-                "I blog about stocks at "
-                "<a href='http://dalal-street.in'>Dalal-Street</a>.")
-
+# Sitemap
+PLUGINS_PATHS = ["pelican-plugins"]
+PLUGINS = ['sitemap', 'random_article', ]
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
 
 # Reading Settings
 TYPOGRIFY = True
@@ -75,31 +86,6 @@ TAGS_SAVE_AS = False
 FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
 RANDOM = 'random/index.html'
 
-# Sitemap
-PLUGINS = ['sitemap', 'random_article', ]
-SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.5,
-        'indexes': 0.5,
-        'pages': 0.5
-    },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly'
-    }
-}
-
-
-# Screen Readers
-FEED_ATOM = None
-FEED_RSS = ('feed.rss')
-FEED_DOMAIN = ("http://feeds.feedburner.com/fullyfaltoo")
-TRANSLATION_FEED = None
-CATEGORY_FEED_ATOM = None
-
-
 # Writing Settings
 THEME = "theme/faltoo"
 READERS = {'html': None, 'rst': None}
@@ -110,8 +96,3 @@ EXTRA_PATH_METADATA = {
     'root/google726cfb66a8285a50.html': {'path': 'google726cfb66a8285a50.html'},
     'root/favicon.ico': {'path': 'favicon.ico'},
 }
-
-
-# Integrations
-DISQUS_SITENAME = "fully-faltoo"
-GOOGLE_ANALYTICS = "UA-2456523-1"
--- publishconf.py
+++ publishconf.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
@@ -9,10 +10,12 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'https://fully-faltoo.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
