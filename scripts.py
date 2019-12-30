1ce21e552e40982860dfd240aacec5108d31d96f
Pratyush Mittal <pratyushmittal@gmail.com>
2019-12-30 09:51:42 +0530

    Added script for preview
    Added a no-dependency script for previewing current content.
    
    We can run it using: python scripts.py preview

---
+++ scripts.py
@@ -0,0 +1,31 @@
+import sys
+import time
+import subprocess
+
+
+def preview():
+    "Shows what will be output"
+    server = subprocess.Popen(['pelican', '--listen'])
+    time.sleep(2)
+    subprocess.run(['open', 'http://localhost:8000'])
+    server.communicate()
+
+
+def commit():
+    "Commit changes"
+    run("pelican content -s publishconf.py")
+    print('git status')
+    print('do you want to continue?')
+    run('git add . -A')
+    run('git commit -m "Auto commiting changes"')
+    run('git push')
+
+
+if __name__ == "__main__":
+    options = {
+        'preview': preview,
+        'commit': commit,
+    }
+    name = sys.argv[1]
+    func = options[name]
+    func()
