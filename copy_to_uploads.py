4950fbb9fc18120e806a17fba786fbcdbeba5348
Pratyush Mittal <pratyushmittal@gmail.com>
2020-02-16 19:03:32 +0530

    Static path in copy_to_uploads
    Alfred actually copies the script into Alfred workflow's folder. So
    relative paths don't work.

--- copy_to_uploads.py
+++ copy_to_uploads.py
@@ -1,15 +1,25 @@
-#!.venv/bin/python
+#!/usr/bin/python3
 import sys
+import pathlib
 import os
 import subprocess
 
+
+def _run(command):
+    subprocess.run(command, check=True)
+
+
 def copy_to_uploads(filepath):
     filename = os.path.basename(filepath)
-    uploads = os.path.join("content", "uploads", filename)
-    subprocess.run(["cp", filepath, uploads])
+    uploads = os.path.join(
+        "/Users/pratyush/Websites/faltoo", "content", "uploads", filename
+    )
+    subprocess.run(["cp", filepath, uploads], check=True)
     # copy new path to clipboard
     new_path = f"/uploads/{filename}"
-    subprocess.run("pbcopy", universal_newlines=True, input=new_path)
+    subprocess.run(
+        "pbcopy", universal_newlines=True, input=new_path, check=True
+    )
 
 
 copy_to_uploads(sys.argv[1])
